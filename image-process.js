8f9c9d07562db0a25bdf266c8cbee8fecd9b01fb
Debian <bitnami@ip-172-26-1-175.ec2.internal>
2022-09-05 18:46:11 +0000

    got image process working

---
+++ image-process.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const { auth, requiresAuth } = require('express-openid-connect');
+
+app.use(
+	auth({
+		authRequired: false,
+		auth0Logout: true,
+		issuerBaseURL: process.env.ISSUER_BASE_URL,
+		baseURL: process.env.BASE_URL,
+		clientID: process.env.CLIENT_ID,
+		secret: process.env.SECRET
+	})
+);
+
+
+app.get('/',(req,res) => {
+	res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile',requiresAuth(), (req,res) => {
+	res.send(JSON.stringify(req.oidc.user))
+});
+
+app.get('/process',(req,res) => {
+	res.send('<img src="https://hsu2apq4v37kmdwhe5poqjhtge0fyrlp.lambda-url.us-east-1.on.aws">')
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log('listening on port ${port}');
+});
