35091653f096338997fcc9b1939c63821b83910a
Brandon Dixon <brandon.dixon@LUBBOCK>
2022-09-28 08:41:02 -0500

    added roadmap route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 const { auth, requiresAuth } = require('express-openid-connect');
 
 var indexRouter = require('./routes/index');
+var roadmapRouter = require('./routes/roadmap');
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/roadmap', roadmapRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
