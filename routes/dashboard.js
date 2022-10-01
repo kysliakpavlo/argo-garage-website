9787c87f40afde9ee691cb5e99b967957db76343
Brandon Dixon <brandon.dixon@TYLER>
2022-09-30 20:45:20 -0500

    created columns in moderate

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var leftColumn = 'Left Column';
+var middleColumn = 'Middle Column';
+var rightColumn = 'Right Column';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('moderate', { title: 'Argonovo | Dashboard' });
+  res.render('moderate', {
+    title: 'Argonovo | Dashboard',
+    left: leftColumn,
+    middle: middleColumn,
+    right: rightColumn
+  });
 });
 
 module.exports = router;
