c09709e98293f21012a60b37809f2d7e3b4f1b95
Brandon Dixon <brandon.dixon@TYLER>
2022-09-30 20:21:09 -0500

    setup dashboar pug template for pages

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard', { title: 'Argonovo | Dashboard' });
+  res.render('moderate', { title: 'Argonovo | Dashboard' });
 });
 
 module.exports = router;
