108fbbf36b49d266234529f79e3d3c40ebb38d51
Brandon Dixon <brandon.dixon@TYLER>
2022-10-01 10:02:01 -0500

    got routes working as dashboard subfolder

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -1,16 +1,8 @@
 var express = require('express');
-var router = express.Router();
+var app = express();
 
-var nextImage = 'https://6fup2zzx27dmctf34en2bnn4om0vrrqa.lambda-url.us-east-1.on.aws/image/04CD8859-095C-449C-BB17-EBA6067258FD_1660426291_CAP_A2FC568E-09D2-4F79-9094-B0BFF45AE8BE.jpg';
-var nextContent = JSON.stringify({"ModerationLabels":[{"Confidence": 56.03160095214844, "Name": "Suggestive", "ParentName": ""}, {"Confidence": 56.03160095214844, "Name": "Revealing Clothes", "ParentName": "Suggestive"}],"Errors":[]},null,2);
+var flaggedRouter	= require('./flagged');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('dashboard/moderate', {
-    title: 'Argonovo | Dashboard',
-    imageURL: nextImage,
-    imageContent: nextContent
-  });
-});
+app.use('/', flaggedRouter);
 
-module.exports = router;
+module.exports = app;
