d58dfa37921544d40d0e56da5f0f666b9fd52e3c
Brandon Dixon <brandon.dixon@TYLER>
2022-10-01 10:46:41 -0500

    added flagged and review views

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 var flaggedRouter	= require('./flagged');
 
 app.use('/', flaggedRouter);
+app.use('/flagged', flaggedRouter);
+app.use('/review', flaggedRouter);
 
 module.exports = app;
