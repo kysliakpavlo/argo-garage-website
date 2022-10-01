fcf89f20d4b65a9898bf8cce366595faabfdaa7d
Brandon Dixon <brandon.dixon@TYLER>
2022-10-01 10:51:32 -0500

    split out flagged and review pages

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -3,9 +3,10 @@ var path = require('path');
 var app = express();
 
 var flaggedRouter	= require('./flagged');
+var reviewRouter	= require('./review');
 
 app.use('/', flaggedRouter);
 app.use('/flagged', flaggedRouter);
-app.use('/review', flaggedRouter);
+app.use('/review', reviewRouter);
 
 module.exports = app;
