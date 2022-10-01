0c6f80ea98a45dd04b17850274e2a86373ba14d7
Brandon Dixon <brandon.dixon@TYLER>
2022-09-30 22:52:06 -0500

    got images and their rotations showing

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,17 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-var leftColumn = 'Left Column';
-var middleColumn = 'Middle Column';
-var rightColumn = 'Right Column';
+var nextImage = 'https://6fup2zzx27dmctf34en2bnn4om0vrrqa.lambda-url.us-east-1.on.aws/image/04CD8859-095C-449C-BB17-EBA6067258FD_1660426291_CAP_A2FC568E-09D2-4F79-9094-B0BFF45AE8BE.jpg';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('moderate', {
     title: 'Argonovo | Dashboard',
-    left: leftColumn,
-    middle: middleColumn,
-    right: rightColumn
+    imageURL: nextImage
   });
 });
 
