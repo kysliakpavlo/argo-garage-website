033561058aa07eb638ac5070e1ca4d02a164d3e3
Brandon Dixon <brandon.dixon@TYLER>
2022-10-01 21:49:45 -0500

    got basics of passing url information to process page done

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
-var path = require('path');
 var app = express();
 
 var flaggedRouter	= require('./flagged');
 var reviewRouter	= require('./review');
+var processRouter	= require('./process');
 
 app.use('/', flaggedRouter);
 app.use('/flagged', flaggedRouter);
 app.use('/review', reviewRouter);
+
+// example /process/id/1323/mod/j
+//  modification image_id=1, done processing
+app.use('/process\/id\/[0-9]+\/mod\/[a-z0-9]', processRouter);
 
 module.exports = app;
---
+++ routes/dashboard/process.js
@@ -0,0 +1,70 @@
+queryTxt = `
+    select now()
+`;
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const { Client } = require('pg');
+
+const client = new Client({
+	host	: process.env.RDS_HOSTNAME,
+	user	: process.env.RDS_USERNAME,
+	port	: process.env.RDS_PORT,
+	password: process.env.RDS_PASSWORD,
+	database: process.env.RDS_DB_NAME
+});
+
+client.connect();
+
+client.query(queryTxt,(err,res)=>{
+	if(!err) {   
+        app.get('/', function(req, res, next) {
+            var baseURL = req.baseUrl;
+            var image_id = baseURL.match(/(?<=\/process\/id\/)[0-9]+(?=\/mod\/[a-z0-9])/g)[0];
+            var mod = baseURL.match(/(?<=\/process\/id\/[0-9]+\/mod\/)[a-z0-9]/g)[0];
+            console.log(baseURL);
+            console.log(image_id);
+            console.log(mod);
+
+            var response = 'Done';
+
+            switch(mod) {
+                case '1':
+                    response = '1';
+                    break;
+                case '2':
+                    response = '2';
+                    break;
+                case '3':
+                    response = '3';
+                    break;
+                case 'b':
+                    response = 'b';
+                    break;
+                case 'f':
+                    response = 'f';
+                    break;
+                case 'j':
+                    response = 'j';
+                    break;
+                default:
+                    response = 'Unknown mod';
+            }
+
+
+
+
+
+
+
+
+            res.send(response);
+        });
+    } else {
+		console.log(err.message);
+    }
+	client.end();
+})
+
+module.exports = app;
