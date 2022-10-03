45daae3f786ec55373f0fc48a5206b440bf5e9ce
Brandon Dixon <brandon.dixon@LUBBOCK>
2022-10-03 16:13:44 -0500

    getting closer to async query

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -5,7 +5,12 @@ var reviewRouter	= require('./review');
 var processRouter	= require('./process');
 
 app.use('/', reviewRouter);
-app.use('/review', reviewRouter);
+
+//processing quees are
+//  0 = review everything with priorites
+//  1 = just review amazon flagged images
+//  2 = just review unflagged (should be good) images
+app.use('/review(\/[012])?', reviewRouter);
 
 // example /process/id/1323/mod/j
 //  modification image_id=1, done processing
--- routes/dashboard/review.js
+++ routes/dashboard/review.js
@@ -1,13 +1,33 @@
-const Router = require('express-promise-router')
-const db = require('../../db')
+const Router = require('express-promise-router');
+const util = require('util');
+const db = require('../../db');
+
 // create a new express-promise-router
 // this has the same API as the normal express router except
 // it allows you to use async functions as route handlers
-const router = new Router()
-// export our router to be mounted by the parent application
-module.exports = router
-router.get('/:id', async (req, res) => {
-  const { id } = req.params
-  const { rows } = await db.query('SELECT * FROM media WHERE id = $1', [id])
+const router = new Router();
+
+router.get('/', async (req, res) => {
+  const endpointURL = 'https://6fup2zzx27dmctf34en2bnn4om0vrrqa.lambda-url.us-east-1.on.aws/image/';
+  const queryTxtA = `SELECT id,image_id,camera_orientation,device_type,blocked,hidden,filtered FROM media `;
+  const queryTxtB = [
+    `WHERE processed_manually=FALSE AND content_type='image'`,
+    `WHERE processed_manually=FALSE AND content_type='image'`,
+    `WHERE processed_manually=FALSE AND content_type='image'`
+  ];
+  const queryTxtC = `ORDER BY blocked DESC,filtered DESC,upload_time DESC LIMIT 1`;
+      
+  var baseURL = req.baseUrl;
+  var process_id = baseURL.match(/(?<=\/review\/)[012]/g);  
+  if ( (process_id != null) && Array.isArray(process_id) && !Number.isNaN(process_id[0])) {
+    process_id = process_id[0];
+  } else {
+    process_id = '0';
+  }
+
+  const { rows } = await db.query(queryTxtA+queryTxtB[process_id]+queryTxtC)
   res.send(rows[0])
 });
+
+// export our router to be mounted by the parent application
+module.exports = router;
