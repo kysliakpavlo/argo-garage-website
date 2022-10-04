76006cdc2be23113ed98ae0fc3ab97b2a70ad240
Brandon Dixon <brandon.dixon@TYLER>
2022-10-04 18:02:02 -0500

    limited review keys

--- public/js/review.js
+++ public/js/review.js
@@ -1,4 +1,6 @@
 // Add event listener on keyup
 document.addEventListener('keyup', (event) => {
-     window.location.replace('/dashboard/process/id/'+db_id+'/mod/'+event.key);
+     if (event.key.match(/[bcfj123]/g)) {
+          window.location.replace('/dashboard/process/id/'+db_id+'/mod/'+event.key);
+     }     
 }, false);
