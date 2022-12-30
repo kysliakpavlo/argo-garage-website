09a5f2b9cda910ad0f0c9f9396c92ca58a849166
Brandon Dixon <brandon.dixon@TYLER>
2022-12-29 21:47:48 -0600

    changed enpoint url

--- routes/dashboard/review.js
+++ routes/dashboard/review.js
@@ -4,7 +4,7 @@ const db = require('../../db');
 const router = new Router();
 
 router.get('/', async (req, res) => {
-  const endpointURL = 'https://4sz7bsq7y7nzg66fxeyvhn5xe40gxyiw.lambda-url.us-east-1.on.aws/';
+  const endpointURL = 'https://rvfewintfuw3vsvscx4wtyupai0eyerk.lambda-url.us-east-1.on.aws/';
   const queryTxtA = `SELECT id,image_id,camera_orientation,device_type,blocked,hidden,filtered FROM media `;
   const queryTxtB = [
     `WHERE processed_manually=FALSE AND content_type='image' AND (blocked=TRUE OR filtered=TRUE) `,
