de03df23f3900cc231dadb8d51e8081f7106a1b6
Brandon Dixon <brandon.dixon@LUBBOCK>
2023-01-05 15:35:11 -0600

    got rotation correction working!

--- routes/dashboard/review.js
+++ routes/dashboard/review.js
@@ -3,7 +3,6 @@ const util = require('util');
 const db = require('../../db');
 const router = new Router();
 
-
 router.get('/', async (req, res) => {
   const endpointURL = 'https://rvfewintfuw3vsvscx4wtyupai0eyerk.lambda-url.us-east-1.on.aws/';
   const queryTxt = `
@@ -42,34 +41,6 @@ router.get('/', async (req, res) => {
     var filtered = rows[0]['filtered'];
     var hidden = rows[0]['hidden'];
     var camera_orientation = rows[0]['camera_orientation'];
-    
-    // Default values for portraitUp
-    var camera_orientation0 = '';
-    var camera_orientation1 = 'rotate90';
-    var camera_orientation2 = 'rotate180';
-    var camera_orientation3 = 'rotate270';
-
-    // Update values if not portrait up
-    switch (camera_orientation) {
-      case 'landscapeRight':
-        camera_orientation0 = 'rotate270';
-        camera_orientation1 = '';
-        camera_orientation2 = 'rotate90';
-        camera_orientation3 = 'rotate180';
-        break;
-      case 'portraitDown':
-        camera_orientation0 = 'rotate180';
-        camera_orientation1 = 'rotate270';
-        camera_orientation2 = '';
-        camera_orientation3 = 'rotate90';
-        break;
-      case 'landscapeLeft':
-        camera_orientation0 = 'rotate90';
-        camera_orientation1 = 'rotate180';
-        camera_orientation2 = 'rotate270';
-        camera_orientation3 = '';
-        break;
-    }
 
     var visualflag = '';
     
@@ -88,10 +59,6 @@ router.get('/', async (req, res) => {
       renderImageID: image_id,
       renderImageURL: endpointURL,
       renderOrientation: camera_orientation,
-      renderOrientation0: camera_orientation0,
-      renderOrientation1: camera_orientation1,
-      renderOrientation2: camera_orientation2,
-      renderOrientation3: camera_orientation3,
       renderJSONdata: JSON.stringify(device_type,null,2),
       renderBlocked: blocked,
       renderFiltered: filtered,
