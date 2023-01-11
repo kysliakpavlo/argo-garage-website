bcc421fafd2df8526e117f2f8170e3cc8092ecef
Brandon Dixon <brandon.dixon@LUBBOCK>
2023-01-11 14:54:36 -0600

    undid rotation fixes

--- routes/dashboard/review.js
+++ routes/dashboard/review.js
@@ -43,6 +43,32 @@ router.get('/', async (req, res) => {
     var hidden = rows[0]['hidden'];
     var camera_orientation = rows[0]['camera_orientation'];
 
+    var camera_orientation0 = '';
+    var camera_orientation1 = 'rotate90';
+    var camera_orientation2 = 'rotate180';
+    var camera_orientation3 = 'rotate270';
+
+    switch (camera_orientation) {
+      case 'landscapeLeft':
+        camera_orientation0 = 'rotate270';
+        camera_orientation1 = '';
+        camera_orientation2 = 'rotate90';
+        camera_orientation3 = 'rotate180';
+        break;
+      case 'portraitDown':
+        camera_orientation0 = 'rotate180';
+        camera_orientation1 = 'rotate270';
+        camera_orientation2 = '';
+        camera_orientation3 = 'rotate90';
+        break;
+      case 'landscapeRight':
+        camera_orientation0 = 'rotate90';
+        camera_orientation1 = 'rotate180';
+        camera_orientation2 = 'rotate270';
+        camera_orientation3 = '';
+        break;
+    }
+
     var visualflag = '';
     
     if (blocked == true) {
@@ -60,6 +86,10 @@ router.get('/', async (req, res) => {
       renderImageID: image_id,
       renderImageURL: endpointURL,
       renderOrientation: camera_orientation,
+      renderOrientation0: camera_orientation0,
+      renderOrientation1: camera_orientation1,
+      renderOrientation2: camera_orientation2,
+      renderOrientation3: camera_orientation3,
       renderJSONdata: JSON.stringify(device_type,null,2),
       renderBlocked: blocked,
       renderFiltered: filtered,
