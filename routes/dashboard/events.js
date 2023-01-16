460d499a328eb240307f9535640d5a979912c4e4
Brandon Dixon <brandon.dixon@TYLER>
2023-01-15 18:36:41 -0600

    removed repetitive include lines

--- routes/dashboard/events.js
+++ routes/dashboard/events.js
@@ -1,8 +1,5 @@
 const Router = require('express-promise-router');
-const util = require('util');
-const db = require('../../db');
 const router = new Router();
-require('dotenv').config();
 
 router.get('/', async (req, res) => {
   res.render('dashboard/events', {
