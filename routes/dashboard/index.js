602c37fcb51c517f9921ab676da3de260f6674f5
Pavlo Kysliak <kysliakpavlo@gmail.com>
2023-01-29 17:01:45 +0200

    check permissions for some pages

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -7,11 +7,23 @@ var reviewRouter	= require('./review');
 var processRouter	= require('./process');
 var searchRouter	= require('./search');
 
+const namespace = (process.env.MANAGEMENT_API_IDENTIFIER || '').replace(/\/$/, '') + '/permissions';
+
+function checkPermissions(perm) {
+  return (req, res, next) => {
+    if (Array.isArray(req.oidc.user[namespace]) && req.oidc.user[namespace].includes(perm)) {
+      next();
+    } else {
+      res.redirect('/dashboard');
+    }
+  };
+}
+
 app.use('/', profileRouter);
 app.use('/profile', profileRouter);
-app.use('/events', eventsRouter);
-app.use('/review', reviewRouter);
-app.use('/search', searchRouter);
+app.use('/events', checkPermissions('create:events'), eventsRouter);
+app.use('/review', checkPermissions('update:review'), reviewRouter);
+app.use('/search', checkPermissions('read:article'), searchRouter);
 
 // example /process/id/1323/mod/j
 //  modification image_id=1, done processing
